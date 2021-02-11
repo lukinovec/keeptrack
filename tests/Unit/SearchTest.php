1733e52ceedf11093e42bf7691cb4171a38d7cc1
lukinovec <lukinovec@gmail.com>
2021-02-11 12:38:31 +0100

    test fix

---
+++ tests/Unit/SearchTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Classes\Search;
+use Illuminate\Support\Collection;
+class SearchTest extends TestCase
+{
+    // use InteractsWithExceptionHandling;
+    /** @test  */
+    public function results_retrieved(): void
+    {
+        return;
+    }
+}
